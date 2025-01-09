1ce3d199da7ee00678ec12356909f05efa693d47
Depermitto <penciller@disroot.org>
2025-01-09 12:06:42 +0100

    fix build on unix, change app icon

--- build.rs
+++ build.rs
@@ -4,6 +4,9 @@ extern crate winres;
 #[cfg(windows)]
 fn main() {
     let mut res = winres::WindowsResource::new();
-    res.set_icon("resources/red_dog_portrait.ico");
+    res.set_icon("resources/doggo.png");
     res.compile().unwrap();
 }
+
+#[cfg(unix)]
+fn main() {}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
+use gui::Roger;
+use iced::{advanced::graphics::image::image_rs::ImageFormat, window::icon, Application, Settings};
+
 mod dog_api;
 mod gui;
 
 fn main() -> iced::Result {
-    use iced::{Settings, Application, window::icon};
-    use gui::Roger;
-
-    let icon_data = include_bytes!("../resources/red_dog_portrait.ico");
-    let icon = icon::from_file_data(icon_data, None).unwrap();
+    let icon = icon::from_file_data(
+        include_bytes!("../resources/doggo.png"),
+        ImageFormat::from_extension("png"),
+    )
+    .unwrap();
     Roger::run(Settings {
         window: iced::window::Settings {
             icon: Some(icon),
